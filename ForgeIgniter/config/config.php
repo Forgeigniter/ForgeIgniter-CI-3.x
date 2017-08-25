6b4d0c2b743076fde6f9383906d60c54438e2d75
Reconix <Reconix@users.noreply.github.com>
2017-08-26 00:21:11 +0100

    Destroy old session data

--- ForgeIgniter/config/config.php
+++ ForgeIgniter/config/config.php
@@ -378,7 +378,7 @@ $config['sess_expiration'] = 14400;
 $config['sess_save_path'] = 'system_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 600;
-$config['sess_regenerate_destroy'] = FALSE;
+$config['sess_regenerate_destroy'] = TRUE;
 
 /*
 |--------------------------------------------------------------------------
