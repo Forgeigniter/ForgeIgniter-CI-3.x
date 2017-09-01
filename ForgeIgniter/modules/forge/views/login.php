f41e95638f084b8a2a5e551f0f671202e828634c
Reconix <reconix@outlook.com>
2017-09-01 07:51:15 +0100

    Anvil - Admin Login

--- ForgeIgniter/modules/forge/views/login.php
+++ ForgeIgniter/modules/forge/views/login.php
@@ -1,44 +1,43 @@
-<?php
-	if(!$this->session->userdata('session_admin')) {
-?>
-
-	<script type="text/javascript">
-	$(function(){
-		$('#username').focus();
-	});
-	</script>
-
-	<h1>Login</h1>
-	
+<body class="hold-transition skin-anvil-light login-page">
+<div class="login-box">
+  <div class="login-logo">
+	<div class="header">
+		<a href="#"><b>Admin - Login</b></a>
+	</div>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+	<!--
+    <p class="login-box-msg">Sign in to start your session</p>
+	-->
 	<?php if ($errors = validation_errors()): ?>
-		<div class="error">
+		<div class="alert alert-danger alert-dismissible">
+			<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+			<h4><i class="icon fa fa-ban"></i> Error</h4>
 			<?php echo $errors; ?>
-		</div>
+        </div>
 	<?php endif; ?>
-	
-	<form action="" method="post" class="default">
-					
-		<label for="username">Username:</label>
-		<input type="text" id="username" name="username" class="formelement" />
 
-		<br class="clear" />
-	
-		<label for="password">Password:</label>
-		<input type="password" id="password" name="password" class="formelement" />
+    <form action="" method="post">
+      <div class="form-group has-feedback">
+        <input type="username" id="username" name="username" class="form-control" placeholder="Username">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" id="password" name="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
 
-		<br class="clear" /><br />
-		<input type="submit" id="login" name="login" value="Login" class="button nolabel" />
-	
-	</form>
+        <!-- /.col -->
+        <div class="col-xs-12" style="padding-top:10px">
+          <button type="submit" id="login" name="login" class="btn btn-primary btn-block" >Sign In</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
 
-<?php
-	} else {
-?>
-
-	<h1>Logout</h1>
-
-	<p><a href="/login/logout/">Click here to logout.</a></p>
-	
-<?php
-	}
-?>
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
