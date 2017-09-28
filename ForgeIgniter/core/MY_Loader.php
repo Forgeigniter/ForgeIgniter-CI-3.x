70586d5c0e5cfad9e2416fd0f26114f263e454fb
Reconix <reconix@outlook.com>
2017-09-28 02:22:10 +0100

    Space Cleanup - Core Loader

--- ForgeIgniter/core/MY_Loader.php
+++ ForgeIgniter/core/MY_Loader.php
@@ -4,7 +4,7 @@
 require APPPATH."third_party/MX/Loader.php";
 
 class MY_Loader extends MX_Loader {
-	
+
     /** Load a module view **/
     public function view($view, $vars = array(), $return = FALSE)
     {
@@ -16,6 +16,6 @@ class MY_Loader extends MX_Loader {
             $view = $_view;
         }
 
-        return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => ((method_exists($this,'_ci_object_to_array')) ? $this->_ci_object_to_array($vars) : $this->_ci_prepare_view_vars($vars)), '_ci_return' => $return));
+        return $this->_ci_load(array('_ci_view' => $view, '_ci_vars' => ((method_exists($this, '_ci_object_to_array')) ? $this->_ci_object_to_array($vars) : $this->_ci_prepare_view_vars($vars)), '_ci_return' => $return));
     }
 }
