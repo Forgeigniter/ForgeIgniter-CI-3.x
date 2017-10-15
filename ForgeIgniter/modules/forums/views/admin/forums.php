e210b3ff540e7b3aa646d89c0e91230b6a054d0d
Reconix <Reconix@users.noreply.github.com>
2017-10-15 01:03:48 +0100

    Forums - Anvil Admin Update
    Updating Forums
    - Add Forum
    - Cats
    - View All ( Forums )

--- ForgeIgniter/modules/forums/views/admin/forums.php
+++ ForgeIgniter/modules/forums/views/admin/forums.php
@@ -1,56 +1,84 @@
-<h1 class="headingleft">Forums</h1>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Forums :
+		<small>Manage</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/dashboard'); ?>"><i class="fa fa-newspaper-o"></i> Forums</a></li>
+		<li class="active">Manage</li>
+	  </ol>
+	</section>
 
-<div class="headingright">
-	<?php if (in_array('forums_edit', $this->permission->permissions)): ?>
-		<a href="<?php echo site_url('/admin/forums/add_forum'); ?>" class="button">Add Forum</a>
-	<?php endif; ?>
-</div>
+	<!-- Main content -->
+    <section class="content container-fluid">
 
-<?php if ($forums): ?>
+		<section class="content">
+			<div class="row">
 
-<?php echo $this->pagination->create_links(); ?>
+				<div class="box box-crey">
+					<div class="box-header with-border">
+					<i class="fa fa-newspaper-o"></i>
+					<h3 class="box-title">Forums</h3>
 
-<table class="default clear">
-	<tr>
-		<th><?php echo order_link('/admin/forums/forums','forumName','Forum'); ?></th>
-		<th><?php echo order_link('/admin/forums/forums','datecreated','Description'); ?></th>
-		<th><?php echo order_link('/admin/forums/forums','active','Active'); ?></th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-	</tr>
-<?php foreach ($forums as $forum): ?>
-	<tr>
-		<td><?php echo (in_array('forums_edit', $this->permission->permissions)) ? anchor('/admin/forums/edit_forum/'.$forum['forumID'], $forum['forumName']) : $forum['forumName']; ?></td>
-		<td><?php echo $forum['description']; ?></td>
-		<td>
-			<?php
-				if ($forum['active']) echo 'Yes';
-				if (!$forum['active']) echo 'No';
-			?>
-		</td>
-		<td><?php echo anchor('/forums/viewforum/'.$forum['forumID'], 'View'); ?></td>	
-		<td class="tiny">
-			<?php if (in_array('forums_edit', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/forums/edit_forum/'.$forum['forumID'], 'Edit'); ?>
-			<?php endif; ?>
-		</td>
-		<td class="tiny">			
-			<?php if (in_array('forums_delete', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/forums/delete_forum/'.$forum['forumID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-			<?php endif; ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-</table>
+					<div class="box-tools">
+						<?php if (in_array('forums_edit', $this->permission->permissions)): ?>
+							<a href="<?= site_url('/admin/forums/add_forum'); ?>" class="mb-xs mt-xs mr-xs btn btn-green">Add Forum</a>
+						<?php endif; ?>
+					</div>
 
-<?php echo $this->pagination->create_links(); ?>
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
 
-<p style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
+					<?php if ($forums): ?>
 
-<?php else: ?>
+					<?php echo $this->pagination->create_links(); ?>
 
-<p class="clear">There are no forums yet.</p>
+					<table class="default clear">
+						<tr>
+							<th><?php echo order_link('/admin/forums/forums','forumName','Forum'); ?></th>
+							<th><?php echo order_link('/admin/forums/forums','datecreated','Description'); ?></th>
+							<th><?php echo order_link('/admin/forums/forums','active','Active'); ?></th>
+							<th class="tiny">&nbsp;</th>
+							<th class="tiny">&nbsp;</th>
+							<th class="tiny">&nbsp;</th>
+						</tr>
+					<?php foreach ($forums as $forum): ?>
+						<tr>
+							<td><?php echo (in_array('forums_edit', $this->permission->permissions)) ? anchor('/admin/forums/edit_forum/'.$forum['forumID'], $forum['forumName']) : $forum['forumName']; ?></td>
+							<td><?php echo $forum['description']; ?></td>
+							<td>
+								<?php
+									if ($forum['active']) echo 'Yes';
+									if (!$forum['active']) echo 'No';
+								?>
+							</td>
+							<td><?php echo anchor('/forums/viewforum/'.$forum['forumID'], 'View'); ?></td>
+							<td class="tiny">
+								<?php if (in_array('forums_edit', $this->permission->permissions)): ?>
+									<?php echo anchor('/admin/forums/edit_forum/'.$forum['forumID'], 'Edit'); ?>
+								<?php endif; ?>
+							</td>
+							<td class="tiny">
+								<?php if (in_array('forums_delete', $this->permission->permissions)): ?>
+									<?php echo anchor('/admin/forums/delete_forum/'.$forum['forumID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+								<?php endif; ?>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+					</table>
 
-<?php endif; ?>
+					<?php echo $this->pagination->create_links(); ?>
 
+					<?php else: ?>
+
+					<p class="clear">There are no forums yet.</p>
+
+					<?php endif; ?>
+
+					</div>
+				</div> <!-- End Box -->
+
+			</div> <!-- End Row -->
+		</section>
