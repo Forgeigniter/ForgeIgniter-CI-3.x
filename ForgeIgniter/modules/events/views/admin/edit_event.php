20012dd262ab6827a16920f29afe3feef46d275d
Reconix <Reconix@users.noreply.github.com>
2017-10-18 08:47:24 +0100

    Events - Anvil Theme Update
    Added basic style for
    - View All
    - Edit
    - Add
    
    Also added excerpt preview

--- ForgeIgniter/modules/events/views/admin/edit_event.php
+++ ForgeIgniter/modules/events/views/admin/edit_event.php
@@ -4,6 +4,11 @@ function preview(el){
 		$('div.preview').html(data);
 	});
 }
+function previewExcerpt(el){
+	$.post('<?php echo site_url('/admin/blog/preview'); ?>', { body: $(el).val() }, function(data){
+		$('div.previewExcerpt').html(data);
+	});
+}
 $(function(){
 	$('input.datebox').datepicker({dateFormat: 'dd M yy'});
 	$('textarea#body').focus(function(){
@@ -12,103 +17,201 @@ $(function(){
 	$('textarea#body').blur(function(){
 		preview(this);
 	});
-	preview($('textarea#body'));	
+	preview($('textarea#body'));
+
+	$('textarea#excerpt').blur(function(){
+		previewExcerpt(this);
+	});
+	previewExcerpt($('textarea#excerpt'));
 });
 </script>
 
-<form name="form" method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="default">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+	Events :
+	<small>Edit</small>
+  </h1>
+  <ol class="breadcrumb">
+	<li><a href="<?= site_url('admin/dashboard'); ?>"><i class="fa fa-calendar"></i> Events</a></li>
+	<li class="active">Edit Event</li>
+  </ol>
+</section>
 
-	<h1 class="headingleft">Edit Event <small>(<a href="<?php echo site_url('/admin/events'); ?>">Back to Events</a>)</small></h1>
-	
-	<div class="headingright">
-		<input type="submit" value="Save Changes" class="button" />
-	</div>
-	
-	<div class="clear"></div>
-	
-	<?php if ($errors = validation_errors()): ?>
-		<div class="error">
+<!-- Main content -->
+<section class="content container-fluid">
+
+	<section class="content">
+
+		<form name="form" method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" class="default">
+
+		<?php if ($errors = validation_errors()): ?>
+		<div class="callout callout-danger">
+			<h4>Warning!</h4>
 			<?php echo $errors; ?>
 		</div>
-	<?php endif; ?>
-	<?php if (isset($message)): ?>
-		<div class="message">
+		<?php endif; ?>
+
+		<?php if (isset($message)): ?>
+		<div class="callout callout-info">
+			<h4>Notice</h4>
 			<?php echo $message; ?>
 		</div>
-	<?php endif; ?>
+		<?php endif; ?>
 
-	<h2 class="underline">Place and Time</h2>
+		<div class="row">
+				<div class="pull-left">
+					<a href="<?= site_url('/admin/events');;?>" class="btn btn-crey margin-bottom" style="margin-left: 15px;">Back to Events</a>
+				</div>
+				<div class="col-md-3 pull-right">
+					<input
+						type="submit"
+						value="Save Changes"
+						class="btn btn-green margin-bottom"
+						style="right:4%;position: absolute;top: 0px;"
+					/>
+				</div>
+		</div>
 
-	<label for="eventName">Event Title:</label>
-	<?php echo @form_input('eventTitle', set_value('eventTitle', $data['eventTitle']), 'id="eventTitle" class="formelement"'); ?>
-	<br class="clear" />
+		<div class="row">
+			<div class="col-md-9">
+				<div class="box box-crey">
+					<div class="box-header with-border">
+						<i class="fa fa-calendar"></i>
+						<h3 class="box-title">Edit Event</h3>
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
 
-	<label for="eventDate">Start Date:</label>
-	<?php echo @form_input('eventDate', date('d M Y', strtotime($data['eventDate'])), 'id="eventDate" class="formelement datebox" readonly="readonly"'); ?>
-	<br class="clear" />
+						<label for="eventName">Event Title:</label>
+						<?php echo @form_input('eventTitle', set_value('eventTitle', $data['eventTitle']), 'id="eventTitle" class="form-control" style="width:50%;"'); ?>
+						<br class="clear" />
 
-	<label for="eventEnd">End Date:</label>
-	<?php echo @form_input('eventEnd', (($data['eventEnd'] > 0) ? date('d M Y', strtotime($data['eventEnd'])) : ''), 'id="eventEnd" class="formelement datebox" readonly="readonly"'); ?>
-	<span class="tip">This is optional and useful if the event goes on for more than one day.</span>
-	<br class="clear" />
+						<label for="excerpt">Introduction (Excerpt):</label>
+						<?php
+							$options = [
+								'name'        => 'excerpt',
+								'id'          => 'excerpt',
+								'value'       => @set_value('excerpt', $data['excerpt']),
+								'rows'        => '5',
+								'cols'        => '10',
+								'style'       => 'width:50%; margin-right:10px;',
+								'class'       => 'form-control formelement code short'
+							];
+							echo form_textarea($options);
+						?>
+						<div class="previewExcerpt"></div>
+						<br class="clear" /><br />
 
-	<label for="time">Time:</label>
-	<?php echo @form_input('time', set_value('time', $data['time']), 'id="time" class="formelement"'); ?>
-	<br class="clear" />
+						<div class="buttons">
+							<a href="#" class="boldbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_bold.png" alt="Bold" title="Bold" /></a>
+							<a href="#" class="italicbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_italic.png" alt="Italic" title="Italic" /></a>
+							<a href="#" class="h1button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h1.png" alt="Heading 1" title="Heading 1"/></a>
+							<a href="#" class="h2button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h2.png" alt="Heading 2" title="Heading 2" /></a>
+							<a href="#" class="h3button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h3.png" alt="Heading 3" title="Heading 3" /></a>
+							<a href="#" class="urlbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_url.png" alt="Insert Link" title="Insert Link" /></a>
+							<a href="<?php echo site_url('/admin/images/browser'); ?>" class="halogycms_imagebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_image.png" alt="Insert Image" title="Insert Image" /></a>
+							<a href="<?php echo site_url('/admin/files/browser'); ?>" class="halogycms_filebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_file.png" alt="Insert File" title="Insert File" /></a>
+							<a href="#" class="previewbutton"><img src="<?php echo $this->config->item('staticPath'); ?>/images/btn_save.png" alt="Preview" title="Preview" /></a>
+						</div>
+						<label for="body">Content (Body):</label>
+						<?php
+							$options = [
+									'name'        => 'description',
+									'id'          => 'body',
+									'value'       => @set_value('description', $data['description']),
+									'rows'        => '15',
+									'cols'        => '10',
+									'style'       => 'width:50%; margin-right:10px;',
+									'class'       => 'form-control formelement code half'
+							];
+							echo form_textarea($options); ?>
+						<div class="preview" style="min-height: 315px;"></div>
+						<br class="clear" /><br />
 
-	<label for="location">Location:</label>
-	<?php echo @form_input('location', set_value('location', $data['location']), 'id="location" class="formelement"'); ?>
-	<br class="clear" /><br />
+					</div><!-- End Box Body -->
+				</div> <!-- End Box -->
+			</div>
 
-	<h2 class="underline">Event Description</h2>	
-	
-	<div class="buttons">
-		<a href="#" class="boldbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_bold.png" alt="Bold" title="Bold" /></a>
-		<a href="#" class="italicbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_italic.png" alt="Italic" title="Italic" /></a>
-		<a href="#" class="h1button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h1.png" alt="Heading 1" title="Heading 1"/></a>
-		<a href="#" class="h2button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h2.png" alt="Heading 2" title="Heading 2" /></a>
-		<a href="#" class="h3button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h3.png" alt="Heading 3" title="Heading 3" /></a>	
-		<a href="#" class="urlbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_url.png" alt="Insert Link" title="Insert Link" /></a>
-		<a href="<?php echo site_url('/admin/images/browser'); ?>" class="halogycms_imagebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_image.png" alt="Insert Image" title="Insert Image" /></a>
-		<a href="<?php echo site_url('/admin/files/browser'); ?>" class="halogycms_filebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_file.png" alt="Insert File" title="Insert File" /></a>
-		<a href="#" class="previewbutton"><img src="<?php echo $this->config->item('staticPath'); ?>/images/btn_save.png" alt="Preview" title="Preview" /></a>	
-	</div>
-	<label for="body">Body:</label>
-	<?php echo @form_textarea('description', set_value('description', $data['description']), 'id="body" class="formelement code half"'); ?>
-	<div class="preview"></div>
-	<br class="clear" /><br />
+			<!-- Right Sidebar -->
+			<div class="col-md-3">
+				<div class="box box-primary">
 
-	<label for="excerpt">Excerpt:</label>
-	<?php echo @form_textarea('excerpt', set_value('excerpt', $data['excerpt']), 'id="excerpt" class="formelement code short"'); ?>
-	<br class="clear" /><br />
+					<div class="box-header with-border">
+						<i class="fa fa-cogs"></i>
+						<h3 class="box-title">Place &amp; Time</h3>
+					</div>
 
-	<h2 class="underline">Publishing</h2>
-	
-	<label for="featured">Featured:</label>
-	<?php 
-		$values = array(
-			0 => 'No',
-			1 => 'Yes',
-		);
-		echo @form_dropdown('featured',$values,set_value('featured', $data['featured']), 'id="featured"'); 
-	?>
-	<br class="clear" />	
+					<div class="box-body" style="padding-bottom:20px;">
 
-	<label for="tags">Tags: <br /></label>
-	<?php echo @form_input('tags', set_value('tags', $data['tags']), 'id="tags" class="formelement"'); ?>
-	<span class="tip">Separate tags with spaces (e.g. &ldquo;event popular london&rdquo;)</span>
-	<br class="clear" />
+						<label for="eventDate">Start Date:</label>
+						<?php echo @form_input('eventDate', date('d M Y', strtotime($data['eventDate'])), 'id="eventDate" class="form-control datebox" readonly="readonly"'); ?>
+						<br class="clear" />
 
-	<label for="published">Publish:</label>
-	<?php 
-		$values = array(
-			1 => 'Yes',
-			0 => 'No (save as draft)',
-		);
-		echo @form_dropdown('published',$values,set_value('published', $data['published']), 'id="published"'); 
-	?>
-	<br class="clear" /><br />
+						<label for="eventEnd">End Date:</label>
+						<?php echo @form_input('eventEnd', (($data['eventEnd'] > 0) ? date('d M Y', strtotime($data['eventEnd'])) : ''), 'id="eventEnd" class="form-control input-style datebox" readonly="readonly"'); ?>
+						<div class="help-tip">
+							<p>Optional, useful if the event goes on for more than one day.</p>
+						</div>
+						<br class="clear" />
 
-	<p class="clear" style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
-	
-</form>
+						<label for="time">Time:</label>
+						<?php echo @form_input('time', set_value('time', $data['time']), 'id="time" class="form-control"'); ?>
+						<br class="clear" />
+
+						<label for="location">Location:</label>
+						<?php echo @form_input('location', set_value('location', $data['location']), 'id="location" class="form-control"'); ?>
+
+					</div>
+				</div> <!-- End Box -->
+
+				<div class="box box-primary">
+
+					<div class="box-header with-border">
+						<i class="fa fa-cogs"></i>
+						<h3 class="box-title">Options &amp; Publishing</h3>
+					</div>
+
+					<div class="box-body" style="padding-bottom:20px;">
+
+						<label for="featured">Featured:</label>
+						<?php
+							$values = array(
+								0 => 'No',
+								1 => 'Yes',
+							);
+							echo @form_dropdown('featured',$values,set_value('featured', $data['featured']), 'id="featured" class="form-control"');
+						?>
+						<br class="clear" />
+
+						<div class="form-group">
+            			<label for="tags">Tags :</label>
+						<?php
+							$options = [
+								'id'			=> 'tags',
+								'name'			=> 'tags',
+								'value'			=> @set_value('tags', $data['tags']),
+								'class'			=> 'form-control',
+								'placeholder'	=> 'Separate tags with a comma,'
+							];
+
+							echo form_input($options); ?>
+            			</div>
+
+						<label for="published">Publish:</label>
+						<?php
+							$values = array(
+								1 => 'Yes',
+								0 => 'No (save as draft)',
+							);
+							echo @form_dropdown('published',$values,set_value('published', $data['published']), 'id="published" class="form-control"');
+						?>
+
+					</div>
+				</div> <!-- End Box -->
+			</div>
+		</div> <!-- End Row -->
+
+		</form>
+
+	</section>
--- ForgeIgniter/modules/events/views/admin/viewall.php
+++ ForgeIgniter/modules/events/views/admin/viewall.php
@@ -1,57 +1,83 @@
-<h1 class="headingleft">Events</h1>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+	Events :
+	<small>Manage</small>
+  </h1>
+  <ol class="breadcrumb">
+	<li><a href="<?= site_url('admin/dashboard'); ?>"><i class="fa fa-calendar"></i> Event</a></li>
+	<li class="active">Manage</li>
+  </ol>
+</section>
 
-<div class="headingright">
-	<?php if (in_array('events_edit', $this->permission->permissions)): ?>
-		<a href="<?php echo site_url('/admin/events/add_event'); ?>" class="button">Add Event</a>
-	<?php endif; ?>
-</div>
+<!-- Main content -->
+<section class="content container-fluid">
 
-<?php if ($events): ?>
+	<section class="content">
+		<div class="row extra-padding">
 
-<?php echo $this->pagination->create_links(); ?>
+			<div class="box box-crey">
+				<div class="box-header with-border">
+					<i class="fa fa-calendar"></i>
+					<h3 class="box-title">Events</h3>
 
-<table class="default clear">
-	<tr>
-		<th><?php echo order_link('/admin/events/viewall','eventtitle','Event'); ?></th>
-		<th><?php echo order_link('/admin/events/viewall','location','Location'); ?></th>		
-		<th><?php echo order_link('/admin/events/viewall','eventDate','Event Start'); ?></th>
-		<th><?php echo order_link('/admin/events/viewall','eventEnd','Event End'); ?></th>
-		<th>Active</th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-	</tr>
-<?php foreach ($events as $event): ?>
-	<tr>
-		<td><?php echo (in_array('events_edit', $this->permission->permissions)) ? anchor('/admin/events/edit_event/'.$event['eventID'], $event['eventTitle']) : $event['eventTitle']; ?></td>
-		<td><?php echo $event['location']; ?></td>		
-		<td><?php echo dateFmt($event['eventDate'], '', FALSE); ?></td>
-		<td><?php echo dateFmt($event['eventEnd'], '', FALSE); ?></td>
-		<td>
-			<?php
-				if (strtotime($event['eventDate']) < time()) echo 'No';
-				else echo 'Yes';
-			?>
-		</td>
-		<td class="tiny">
-			<?php if (in_array('events_edit', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/events/edit_event/'.$event['eventID'], 'Edit'); ?>
-			<?php endif; ?>
-		</td>
-		<td class="tiny">			
-			<?php if (in_array('events_delete', $this->permission->permissions)): ?>
-				<?php echo anchor('/admin/events/delete_event/'.$event['eventID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-			<?php endif; ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-</table>
+					<div class="box-tools">
+						<?php if (in_array('events_edit', $this->permission->permissions)): ?>
+						<a href="<?= site_url('/admin/events/add_event'); ?>" class="mb-xs mt-xs mr-xs btn btn-green">Add Event</a>
+						<?php endif; ?>
+					</div>
+				</div>
+				<!-- /.box-header -->
+				<div class="box-body table-responsive no-padding">
 
-<?php echo $this->pagination->create_links(); ?>
+					<?php if ($events): ?>
 
-<p style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
+					<?php echo $this->pagination->create_links(); ?>
 
-<?php else: ?>
+					<table class="table table-hover">
+						<tr>
+							<th><?php echo order_link('/admin/events/viewall','eventtitle','Event'); ?></th>
+							<th><?php echo order_link('/admin/events/viewall','location','Location'); ?></th>
+							<th><?php echo order_link('/admin/events/viewall','eventDate','Event Start'); ?></th>
+							<th><?php echo order_link('/admin/events/viewall','eventEnd','Event End'); ?></th>
+							<th>Active</th>
+							<th class="tiny">&nbsp;</th>
+						</tr>
+					<?php foreach ($events as $event): ?>
+						<tr>
+							<td><?php echo (in_array('events_edit', $this->permission->permissions)) ? anchor('/admin/events/edit_event/'.$event['eventID'], $event['eventTitle']) : $event['eventTitle']; ?></td>
+							<td><?php echo $event['location']; ?></td>
+							<td><?php echo dateFmt($event['eventDate'], '', FALSE); ?></td>
+							<td><?php echo dateFmt($event['eventEnd'], '', FALSE); ?></td>
+							<td>
+								<?php
+									if (strtotime($event['eventDate']) < time()) echo 'No';
+									else echo 'Yes';
+								?>
+							</td>
+							<td class="tiny">
+								<?php if (in_array('events_edit', $this->permission->permissions)): ?>
+									<?php echo anchor('/admin/events/edit_event/'.$event['eventID'], '<i class="fa fa-pencil"></i>', 'class="table-edit"'); ?>
+								<?php endif; ?>
 
-<p class="clear">There are no events yet.</p>
+								<?php if (in_array('events_delete', $this->permission->permissions)): ?>
+									<?php echo anchor('/admin/events/delete_event/'.$event['eventID'], '<i class="fa fa-trash-o"></i>', 'class="table-delete"', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+								<?php endif; ?>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+					</table>
 
-<?php endif; ?>
+					<?php echo $this->pagination->create_links(); ?>
+
+					<?php else: ?>
+
+					<p class="clear">There are no events yet.</p>
+
+					<?php endif; ?>
+
+				</div><!-- End Box Body -->
+			</div> <!-- End Box -->
+
+		</div> <!-- End Row -->
+	</section>
