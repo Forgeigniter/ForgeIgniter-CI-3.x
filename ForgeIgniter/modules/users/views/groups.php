678fe1381f94a6e993ad3dda2d97498d4180b255
Reconix <Reconix@users.noreply.github.com>
2017-11-06 23:49:32 +0000

    Users - Groups - Anvil Theme Update

--- ForgeIgniter/modules/users/views/groups.php
+++ ForgeIgniter/modules/users/views/groups.php
@@ -1,45 +1,71 @@
-<h1 class="headingleft">User Groups</h1>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Users :
+		<small>Groups</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/users'); ?>"><i class="fa fa-users"></i> Users</a></li>
+		<li class="active">User Groups</li>
+	  </ol>
+	</section>
 
-<div class="headingright">
+	<!-- Main content -->
+    <section class="content container-fluid">
 
-	<?php if (in_array('users', $this->permission->permissions)): ?>
-		<a href="<?php echo site_url('/admin/users'); ?>" class="button blue">Users</a>
-	<?php endif; ?>
+		<section class="content">
+			<div class="row extra-padding">
 
-	<?php if (in_array('users_groups', $this->permission->permissions)): ?>
-		<a href="<?php echo site_url('/admin/users/add_group'); ?>" class="button">Add Group</a>
-	<?php endif; ?>
-</div>
+				<div class="box box-crey">
+					<div class="box-header with-border">
+						<i class="fa fa-users"></i>
+						<h3 class="box-title">User Groups</h3>
 
-<?php if ($permission_groups): ?>
+						<div class="box-tools">
 
-<?php echo $this->pagination->create_links(); ?>
+							<?php if (in_array('users_groups', $this->permission->permissions)): ?>
+								<a href="<?= site_url('/admin/users/add_group'); ?>" class="mb-xs mt-xs mr-xs btn btn-green" style="margin-left: 15px;">Add Group</a>
+							<?php endif; ?>
 
-<table class="default clear">
-	<tr>
-		<th><?php echo order_link('/admin/users/viewall','groupName','Group name'); ?></th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>		
-	</tr>
-<?php foreach ($permission_groups as $group): ?>
-	<tr>
-		<td><?php echo (in_array('users_groups', $this->permission->permissions)) ? anchor('/admin/users/edit_group/'.$group['groupID'], $group['groupName']) : $group['groupName']; ?></td>
-		<td class="tiny">
-			<?php echo anchor('/admin/users/edit_group/'.$group['groupID'], 'Edit'); ?>
-		</td>
-		<td class="tiny">
-			<?php echo anchor('/admin/users/delete_group/'.$group['groupID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
-		</td>
-	</tr>
-<?php endforeach; ?>
-</table>
+							<?php if (in_array('users', $this->permission->permissions)): ?>
+								<a href="<?= site_url('/admin/users'); ?>" class="mb-xs mt-xs mr-xs btn btn-blue" style="margin-left: 15px;">Users</a>
+							<?php endif; ?>
 
-<?php echo $this->pagination->create_links(); ?>
+						</div>
+					</div> <!-- End Box Header -->
 
-<p style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
+					<div class="box-body table-responsive no-padding">
 
-<?php else: ?>
+						<?php if ($permission_groups): ?>
 
-<p class="clear">There are no permission groups set up yet.</p>
+						<?php echo $this->pagination->create_links(); ?>
 
-<?php endif; ?>
+						<table class="table table-hover">
+							<tr>
+								<th><?php echo order_link('/admin/users/groups','groupName','Group name'); ?></th>
+								<th class="tiny">&nbsp;</th>
+							</tr>
+						<?php foreach ($permission_groups as $group): ?>
+							<tr>
+								<td><?php echo (in_array('users_groups', $this->permission->permissions)) ? anchor('/admin/users/edit_group/'.$group['groupID'], $group['groupName']) : $group['groupName']; ?></td>
+								<td class="tiny">
+									<?php echo anchor('/admin/users/edit_group/'.$group['groupID'], '<i class="fa fa-pencil"></i>', 'class="table-edit"'); ?>
+									<?php echo anchor('/admin/users/delete_group/'.$group['groupID'], '<i class="fa fa-trash-o"></i>', 'class="table-delete"', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+						</table>
+
+						<?php echo $this->pagination->create_links(); ?>
+
+						<?php else: ?>
+
+						<p class="clear">There are no permission groups set up yet.</p>
+
+						<?php endif; ?>
+
+					</div> <!-- End Box body -->
+				</div> <!-- End Box -->
+
+			</div> <!-- End Row -->
+		</section>
