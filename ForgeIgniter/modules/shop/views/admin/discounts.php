88e2501b088be3c2abfd7a05aa12d4feeb6dd1b7
Reconix <Reconix@users.noreply.github.com>
2017-11-16 07:32:48 +0000

    Shop - Discounts - Anvil Theme Update
    basic layout.

--- ForgeIgniter/modules/shop/views/admin/discounts.php
+++ ForgeIgniter/modules/shop/views/admin/discounts.php
@@ -1,56 +1,110 @@
 <script type="text/javascript">
 $(function(){
-	$.listen('click', 'a.showform', function(event){showForm(this,event);});
-	$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
+	// $.listen('click', 'a.showform', function(event){showForm(this,event);});
+	//$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
 });
 </script>
 
-<h1 class="headingleft">Discount Codes</h1>
+<script type="text/javascript">
+// Lets not mess with models just yet.
+/*
+	$(document).ready(function(e) {
+	$('.bootpopup').click(function(){
+	var frametarget = $(this).attr('href');
+	targetmodal = '#myModal';
+		$('#modeliframe').attr("src", frametarget );
+	});
+});
+*/
+/*
+$(function(){
+	$('a.showform').click(function(event){
+		event.preventDefault();
+		$("/admin/shop/add_discount").show();
+		//showForm(this,event);
+	});
 
-<div class="headingright">	
-	<a href="<?php echo site_url('/admin/shop/add_discount'); ?>" class="showform button blue">Add Discount</a>
-</div>
+});
+*/
 
-<div class="clear"></div>
-<div class="hidden"></div>
+</script>
 
-<?php if ($shop_discounts): ?>
 
-<?php echo $this->pagination->create_links(); ?>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Shop :
+		<small>Discount Codes</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/shop'); ?>"><i class="fa fa-shopping-cart"></i> Shop</a></li>
+		<li class="active">Discount Codes</li>
+	  </ol>
+	</section>
 
-<table class="default clear">
-	<tr>
-		<th>Code</th>
-		<th>Calculated On</th>
-		<th>Discount</th>
-		<th>Expiry Date</th>
-		<th class="tiny">&nbsp;</th>
-		<th class="tiny">&nbsp;</th>
-	</tr>
-<?php foreach ($shop_discounts as $discount): ?>
-	<tr>
-		<td><?php echo anchor('/admin/shop/edit_discount/'.$discount['discountID'], $discount['code'], 'class="showform"'); ?></td>
-		<td><?php
-			if ($discount['type'] == 'P') echo 'Product';
-			elseif ($discount['type'] == 'C') echo 'Category';
-			else echo 'Total';
-		?></td>
-		<td><?php echo ($discount['modifier'] == 'A') ? currency_symbol().number_format($discount['discount'],2) : $discount['discount'].'%'; ?></td>
-		<td><?php echo (strtotime($discount['expiryDate']) < time()) ? 
-			'<span style="color:red;">'.dateFmt($discount['expiryDate']).'</span>' : 
-			'<span style="color:green;">'.dateFmt($discount['expiryDate']).'</span>'; ?></td>
-		<td><?php echo anchor('/admin/shop/edit_discount/'.$discount['discountID'], 'Edit', 'class="showform"'); ?></td>
-		<td><?php echo anchor('/admin/shop/delete_discount/'.$discount['discountID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?></td>
-	</tr>
-<?php endforeach; ?>
-</table>
+	<!-- Main content -->
+	<section class="content container-fluid">
 
-<?php echo $this->pagination->create_links(); ?>
+		<div class="row extra-padding">
+			<div class="box box-crey">
+				<div class="box-header with-border">
+					<i class="fa fa-shopping-cart"></i>
+					<h3 class="box-title">Discount Codes</h3>
+					<div class="box-tools">
+						<a href="<?= site_url('/admin/shop/add_discount'); ?>" class="mb-xs mt-xs mr-xs btn btn-green">Add Discount</a>
+					</div>
+				</div>
 
-<p style="text-align: right;"><a href="#" class="button grey" id="totop">Back to top</a></p>
+				<div class="box-body table-responsive no-padding">
 
-<?php else: ?>
+					<div class="clear"></div>
+					<div class="hidden">
+						<p class="hide"><a href="#">x</a></p>
+						<div class="inner"></div>
+					</div>
 
-<p>You have not set up any discount codes yet.</p>
+					<?php if ($shop_discounts): ?>
 
-<?php endif; ?>
+					<?php echo $this->pagination->create_links(); ?>
+
+					<table class="table table-hover">
+						<tr>
+							<th>Code</th>
+							<th>Calculated On</th>
+							<th>Discount</th>
+							<th>Expiry Date</th>
+							<th class="tiny">&nbsp;</th>
+						</tr>
+					<?php foreach ($shop_discounts as $discount): ?>
+						<tr>
+							<td><?php echo anchor('/admin/shop/edit_discount/'.$discount['discountID'], $discount['code'], 'class="showform"'); ?></td>
+							<td><?php
+								if ($discount['type'] == 'P') echo 'Product';
+								elseif ($discount['type'] == 'C') echo 'Category';
+								else echo 'Total';
+							?></td>
+							<td><?php echo ($discount['modifier'] == 'A') ? currency_symbol().number_format($discount['discount'],2) : $discount['discount'].'%'; ?></td>
+							<td><?php echo (strtotime($discount['expiryDate']) < time()) ?
+								'<span style="color:red;">'.dateFmt($discount['expiryDate']).'</span>' :
+								'<span style="color:green;">'.dateFmt($discount['expiryDate']).'</span>'; ?></td>
+							<td>
+								<?php echo anchor('/admin/shop/edit_discount/'.$discount['discountID'], '<i class="fa fa-pencil"></i>', 'class="table-edit"'); ?>
+								<?php echo anchor('/admin/shop/delete_discount/'.$discount['discountID'], '<i class="fa fa-trash-o"></i>', 'class="table-delete"', 'onclick="return confirm(\'Are you sure you want to delete this?\')"'); ?>
+							</td>
+						</tr>
+					<?php endforeach; ?>
+					</table>
+
+					<?php echo $this->pagination->create_links(); ?>
+
+					<?php else: ?>
+
+					<p>You have not set up any discount codes yet.</p>
+
+					<?php endif; ?>
+
+				</div> <!-- End box body -->
+			</div> <!-- End Box -->
+
+		</div> <!-- End row -->
+	</section>
