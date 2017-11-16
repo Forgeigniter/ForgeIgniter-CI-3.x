4e72309867c8455beac66d5ad32a1af8229ce0f7
Reconix <Reconix@users.noreply.github.com>
2017-11-16 12:50:17 +0000

    Shop - Shipping Bands - Anvil Theme Update
    basic layout.

--- ForgeIgniter/modules/shop/views/admin/bands.php
+++ ForgeIgniter/modules/shop/views/admin/bands.php
@@ -1,39 +1,66 @@
 <script type="text/javascript">
 $(function(){
-	$.listen('click', 'a.showform', function(event){showForm(this,event);});
-	$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
+	//$.listen('click', 'a.showform', function(event){showForm(this,event);});
+	//$.listen('click', 'input#cancel', function(event){hideForm(this,event);});
 });
 </script>
 
-<h1 class="headingleft">Shipping Bands</h1>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Shop :
+		<small>Shipping Bands</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/shop'); ?>"><i class="fa fa-shopping-cart"></i> Shop</a></li>
+		<li class="active">Shipping Bands</li>
+	  </ol>
+	</section>
 
-<div class="headingright">	
-	<a href="<?php echo site_url('/admin/shop/add_band'); ?>" class="showform button blue">Add Band</a>
-</div>
+	<!-- Main content -->
+	<section class="content container-fluid">
 
-<div class="clear"></div>
-<div class="hidden"></div>
+		<div class="row extra-padding">
+			<div class="box box-crey">
+				<div class="box-header with-border">
+					<i class="fa fa-shopping-cart"></i>
+					<h3 class="box-title">Shipping Bands</h3>
+					<div class="box-tools">
+						<a href="<?= site_url('/admin/shop/add_band'); ?>" class="mb-xs mt-xs mr-xs btn btn-green">Add Band</a>
+					</div>
+				</div>
 
-<?php if ($shop_bands): ?>
-<table class="default">
-	<tr>
-		<th>Multiplier</th>
-		<th>Name</th>
-		<th class="tiny"></th>		
-		<th class="tiny"></th>
-	</tr>
-	<?php foreach($shop_bands as $band): ?>
-		<tr>
-			<td><?php echo $band['multiplier']; ?> <small>x</small></td>
-			<td><?php echo $band['bandName']; ?></td>
-			<td><?php echo anchor('/admin/shop/edit_band/'.$band['bandID'], 'Edit', 'class="showform"'); ?></td>
-			<td><?php echo anchor('/admin/shop/delete_band/'.$band['bandID'], 'Delete', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?></td>
-		</tr>
-	<?php endforeach; ?>
-</table>
+				<div class="box-body table-responsive no-padding">
 
-<?php else: ?>
+					<div class="hidden"></div>
 
-<p>You have not yet set up any shipping bands yet.</p>
+					<?php if ($shop_bands): ?>
+					<table class="table table-hover">
+						<tr>
+							<th>Multiplier</th>
+							<th>Name</th>
+							<th class="tiny"></th>
+						</tr>
+						<?php foreach($shop_bands as $band): ?>
+							<tr>
+								<td><?php echo $band['multiplier']; ?> <small>x</small></td>
+								<td><?php echo $band['bandName']; ?></td>
+								<td>
+									<?php echo anchor('/admin/shop/edit_band/'.$band['bandID'], '<i class="fa fa-pencil"></i>', 'class="table-edit"'); ?>
+									<?php echo anchor('/admin/shop/delete_band/'.$band['bandID'], '<i class="fa fa-trash-o"></i>', 'class="table-delete"', 'onclick="return confirm(\'Are you sure you want to delete this?\');"'); ?>
+								</td>
+							</tr>
+						<?php endforeach; ?>
+					</table>
 
-<?php endif; ?>
+					<?php else: ?>
+
+					<p>You have not yet set up any shipping bands yet.</p>
+
+					<?php endif; ?>
+
+				</div> <!-- End box body -->
+			</div> <!-- End Box -->
+
+		</div> <!-- End row -->
+	</section>
