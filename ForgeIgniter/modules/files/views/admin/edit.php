11ae3fd1f3fae9d7225824012d574fdcb7a793cf
Reconix <Reconix@users.noreply.github.com>
2017-11-21 16:36:47 +0000

    Images / Files - Edit File - Anvil Theme Update
    basic layout.

--- ForgeIgniter/modules/files/views/admin/edit.php
+++ ForgeIgniter/modules/files/views/admin/edit.php
@@ -1,32 +1,66 @@
-<?php if ($errors = validation_errors()): ?>
-	<div class="error clear">
-		<?php echo $errors; ?>
-	</div>
-<?php endif; ?>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Images / Files :
+		<small>Edit File</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/files'); ?>"><i class="fa fa-file-o"></i> Images / Files</a></li>
+		<li class="active">Edit File</li>
+	  </ol>
+	</section>
 
-<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" enctype="multipart/form-data" class="default" style="width: 450px; float: left;">
+	<!-- Main content -->
+	<section class="content container-fluid">
 
-	<label for="fileRef">Reference:</label>
-	<?php echo @form_input('fileRef', $data['fileRef'], 'class="formelement" id="fileRef"'); ?>
-	<br class="clear" />
+		<section class="content">
+			<div class="row extra-padding">
 
-	<label for="folderID">Folder: <small>[<a href="<?php echo site_url('/admin/files/folders'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
-	<?php
-		$options[0] = 'No Folder';
-		if ($folders):
-			foreach ($folders as $folderID):
-				$options[$folderID['folderID']] = $folderID['folderName'];
-			endforeach;
-		endif;
-			
-		echo @form_dropdown('folderID',$options,set_value('folderID', $data['folderID']),'id="folderID" class="formelement"');
-	?>	
-	<br class="clear" /><br />
+			<?php if ($errors = validation_errors()): ?>
+			<div class="callout callout-danger">
+				<h4>Warning!</h4>
+				<?php echo $errors; ?>
+			</div>
+			<?php endif; ?>
 
-	<input type="submit" value="Save Changes" class="button nolabel" id="submit" />
-	<a href="<?php echo $this->session->userdata('lastPage'); ?>" class="button cancel grey">Cancel</a>
-	<br class="clear" />
-	
-</form>
+			<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" enctype="multipart/form-data">
 
-<br class="clear" />
+				<div class="box box-crey">
+					<div class="box-header with-border">
+					<i class="fa fa-file-o"></i>
+					<h3 class="box-title">Edit File</h3>
+						<div class="box-tools">
+
+							<?php if ($this->site->config['plan'] = 0 || $this->site->config['plan'] = 6 || (($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6) && $quota < $this->site->plans['storage'])): ?>
+							<input type="submit" class="mb-xs mt-xs mr-xs btn btn-green" style="float:none;" value="Save Changes"/>
+							<?php endif; ?>
+
+						</div>
+					</div>
+
+					<div class="box-body">
+
+							<label for="fileRef">Reference:</label>
+							<?php echo @form_input('fileRef', $data['fileRef'], 'class="formelement" id="fileRef"'); ?>
+							<br class="clear" /><br />
+
+							<label for="folderID">Folder: <small>[<a href="<?php echo site_url('/admin/files/folders'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
+							<?php
+								$options[0] = 'No Folder';
+								if ($folders):
+									foreach ($folders as $folderID):
+										$options[$folderID['folderID']] = $folderID['folderName'];
+									endforeach;
+								endif;
+
+								echo @form_dropdown('folderID',$options,set_value('folderID', $data['folderID']),'id="folderID" class="formelement"');
+							?>
+							<br class="clear" /><br />
+
+					</div> <!-- end box body -->
+
+				</div> <!-- end box -->
+			</form>
+			</div> <!-- end row -->
+		</section>
+	</section>
