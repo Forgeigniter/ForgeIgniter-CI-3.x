406900d8195f393c7d45c23731387cbb757394c5
Reconix <Reconix@users.noreply.github.com>
2017-11-21 16:41:06 +0000

    Images / Files - Edit Image - Anvil Theme Update
    Basic layout.

--- ForgeIgniter/modules/images/views/admin/edit.php
+++ ForgeIgniter/modules/images/views/admin/edit.php
@@ -1,74 +1,110 @@
-<?php if ($errors = validation_errors()): ?>
-	<div class="error clear">
-		<?php echo $errors; ?>
-	</div>
-<?php endif; ?>
+	<!-- Content Header (Page header) -->
+	<section class="content-header">
+	  <h1>
+		Images / Files :
+		<small>Edit Image</small>
+	  </h1>
+	  <ol class="breadcrumb">
+		<li><a href="<?= site_url('admin/files'); ?>"><i class="fa fa-file-o"></i> Images / Files</a></li>
+		<li class="active">Edit Image</li>
+	  </ol>
+	</section>
 
-<div style="float: right;">
-	<?php
-		$image = $this->uploads->load_image($data['imageRef']);
-		$thumb = $this->uploads->load_image($data['imageRef'], true);
-		$imagePath = $image['src'];
-		$imageThumbPath = $thumb['src'];
-	?>	
-	<?php echo ($thumb = display_image(base_url(), $imageThumbPath, $data['imageName'], 100, 'class="pic" ')) ? $thumb : display_image(base_url(), $imagePath, $data['imageName'], 100, 'class="pic"'); ?>		
-</div>
+	<!-- Main content -->
+	<section class="content container-fluid">
 
-<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" enctype="multipart/form-data" class="default" style="width: 450px; float: left;">
+		<section class="content">
+			<div class="row extra-padding">
 
-	<label for="image">Image:</label>
-	<div class="uploadfile">
-		<?php echo @form_upload('image', '', 'size="16" id="image"'); ?>
-	</div>
-	<br class="clear" />
+			<?php if ($errors = validation_errors()): ?>
+			<div class="callout callout-danger">
+				<h4>Warning!</h4>
+				<?php echo $errors; ?>
+			</div>
+			<?php endif; ?>
 
-	<label for="folderID">Folder: <small>[<a href="<?php echo site_url('/admin/images/folders'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
-	<?php
-		$options[0] = 'No Folder';
-		if ($folders):
-			foreach ($folders as $folderID):
-				$options[$folderID['folderID']] = $folderID['folderName'];
-			endforeach;
-		endif;
-			
-		echo @form_dropdown('folderID',$options,set_value('folderID', $data['folderID']),'id="folderID" class="formelement"');
-	?>	
-	<br class="clear" />
-	
-	<label for="imageName">Description:</label>
-	<?php echo @form_input('imageName', $data['imageName'], 'class="formelement" id="imageName"'); ?>
-	<br class="clear" />
+			<form method="post" action="<?php echo site_url($this->uri->uri_string()); ?>" enctype="multipart/form-data">
 
-	<label for="imageRef">Reference:</label>
-	<?php echo @form_input('imageRef', $data['imageRef'], 'class="formelement" id="imageRef"'); ?>
-	<br class="clear" />
+				<div class="box box-crey">
+					<div class="box-header with-border">
+					<i class="fa fa-file-o"></i>
+					<h3 class="box-title">Edit Image</h3>
+						<div class="box-tools">
 
-	<label for="class">Display:</label>
-	<?php
-		$values = array(
-			'default' => 'Default',	
-			'left' => 'Left Align',
-			'center' => 'Center Align',			
-			'right' => 'Right Align',
-			'bordered' => 'Border',
-			'bordered left' => 'Border - Left Align',
-			'bordered center' => 'Border - Center Align',			
-			'bordered right' => 'Border - Right Align',
-			'full' => 'Full Width',			
-			'' => 'No Style'
-		);					
-		echo @form_dropdown('class',$values,$data['class'], 'class="formelement"'); 
-	?>
-	<br class="clear" />
+							<?php if ($this->site->config['plan'] = 0 || $this->site->config['plan'] = 6 || (($this->site->config['plan'] > 0 && $this->site->config['plan'] < 6) && $quota < $this->site->plans['storage'])): ?>
+							<input type="submit" class="mb-xs mt-xs mr-xs btn btn-green" style="float:none;" value="Save Changes"/>
+							<?php endif; ?>
 
-	<label for="maxsize">Max Size (px):</label>
-	<?php echo @form_input('maxsize', set_value('maxsize', (($data['maxsize']) ? $data['maxsize'] : '')), 'class="formelement" id="maxsize"'); ?>
-	<br class="clear" /><br />	
+						</div>
+					</div>
 
-	<input type="submit" value="Save Changes" class="button nolabel" id="submit" />
-	<a href="<?php echo $this->session->userdata('lastPage'); ?>" class="button cancel grey">Cancel</a>
-	<br class="clear" />
-	
-</form>
+					<div class="box-body">
 
-<br class="clear" />
+						<div style="float: right;">
+							<?php
+								$image = $this->uploads->load_image($data['imageRef']);
+								$thumb = $this->uploads->load_image($data['imageRef'], true);
+								$imagePath = $image['src'];
+								$imageThumbPath = $thumb['src'];
+							?>
+							<a href="<?= base_url($imagePath); ?>" title="<?php echo $image['imageName']; ?>" class="lightbox">
+								<img src="<?= base_url($imageThumbPath); ?>" class="pic" />
+							</a>
+						</div>
+
+							<label for="image">Image:</label>
+							<div class="uploadfile">
+								<?php echo form_upload('image', '', 'size="16" id="image"'); ?>
+							</div>
+							<br /><br />
+
+							<label for="folderID">Folder: <small>[<a href="<?php echo site_url('/admin/images/folders'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
+							<?php
+								$options[0] = 'No Folder';
+								if ($folders):
+									foreach ($folders as $folderID):
+										$options[$folderID['folderID']] = $folderID['folderName'];
+									endforeach;
+								endif;
+
+								echo @form_dropdown('folderID',$options,set_value('folderID', $data['folderID']),'id="folderID" class="formelement"');
+							?>
+							<br /><br />
+
+							<label for="imageName">Description:</label>
+							<?php echo @form_input('imageName', $data['imageName'], 'class="formelement" id="imageName"'); ?>
+							<br /><br />
+
+							<label for="imageRef">Reference:</label>
+							<?php echo @form_input('imageRef', $data['imageRef'], 'class="formelement" id="imageRef"'); ?>
+							<br /><br />
+
+							<label for="class">Display:</label>
+							<?php
+								$values = array(
+									'default' => 'Default',
+									'left' => 'Left Align',
+									'center' => 'Center Align',
+									'right' => 'Right Align',
+									'bordered' => 'Border',
+									'bordered left' => 'Border - Left Align',
+									'bordered center' => 'Border - Center Align',
+									'bordered right' => 'Border - Right Align',
+									'full' => 'Full Width',
+									'' => 'No Style'
+								);
+								echo @form_dropdown('class',$values,$data['class'], 'class="formelement"');
+							?>
+							<br /><br />
+
+							<label for="maxsize">Max Size (px):</label>
+							<?php echo @form_input('maxsize', set_value('maxsize', (($data['maxsize']) ? $data['maxsize'] : '')), 'class="formelement" id="maxsize"'); ?>
+							<br /><br />
+
+					</div> <!-- end box body -->
+
+				</div> <!-- end box -->
+			</form>
+			</div> <!-- end row -->
+		</section>
+	</section>
