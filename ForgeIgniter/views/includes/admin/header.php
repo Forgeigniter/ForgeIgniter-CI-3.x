c43dbaa75a5927b0dee77617e29fe25e2a09365f
Reconix <Reconix@users.noreply.github.com>
2017-12-03 09:41:00 +0000

    Anvil - Adding a avatar placeholder
    Avatar placeholder until we add correct function.

--- ForgeIgniter/views/includes/admin/header.php
+++ ForgeIgniter/views/includes/admin/header.php
@@ -216,14 +216,14 @@ Not implemented yet.
             <!-- Menu Toggle Button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <!-- The user image in the navbar-->
-              <img src="<?=PATH['theme'];?>anvil/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="<?=PATH['theme'];?>anvil/img/avatar.png" class="user-image" alt="User Image">
               <!-- hidden-xs hides the username on small devices so only the image appears. -->
               <span class="hidden-xs"><?= $this->session->userdata('username'); ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- The user image in the menu -->
               <li class="user-header">
-                <img src="<?=PATH['theme'];?>anvil/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="<?=PATH['theme'];?>anvil/img/avatar.png" class="img-circle" alt="User Image">
 
                 <p>
                   <?= $this->session->userdata('username'); ?><!-- - Site Role / Rank -->
@@ -255,7 +255,7 @@ Not implemented yet.
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?=PATH['theme'];?>anvil/img/user2-160x160.jpg" class="img-rounded" alt="User Image">
+          <img src="<?=PATH['theme'];?>anvil/img/avatar.png" class="img-rounded" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?= $this->session->userdata('username'); ?></p>
