ff5e7f0c88e6fff3331a63b2d1ddb4bcadecf059
Reconix <Reconix@users.noreply.github.com>
2017-12-03 09:57:34 +0000

    Main Config - Set base url
    On a couple of tests this worked fine.
    If problems continue have it set via core installer.
    Should also fix #22

--- ForgeIgniter/config/config.php
+++ ForgeIgniter/config/config.php
@@ -22,9 +22,11 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$site_base_url = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '').'://'.$_SERVER['HTTP_HOST'].str_replace('//','/',dirname($_SERVER['SCRIPT_NAME']).'/');
+$site_base_url = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http').'://'.$_SERVER['HTTP_HOST'].str_replace('//','/',dirname($_SERVER['SCRIPT_NAME']).'/');
 
-$config['base_url'] = 'http://localhost/';
+$config['base_url'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 'https' : 'http';
+$config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
 
 /*
