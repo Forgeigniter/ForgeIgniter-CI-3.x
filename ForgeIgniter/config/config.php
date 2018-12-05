a085e3fa6a10500ece6be82ec8c74250a0aaa0b5
Reconix <Reconix@users.noreply.github.com>
2018-12-05 05:57:01 +0000

    Manually Set URL

--- ForgeIgniter/config/config.php
+++ ForgeIgniter/config/config.php
@@ -28,6 +28,9 @@ $config['base_url'] = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 
 $config['base_url'] .= '://' . $_SERVER['HTTP_HOST'];
 $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
 
+// If all else fails, manually set URL
+//$config['base_url']	= '';
+
 
 /*
 |--------------------------------------------------------------------------
