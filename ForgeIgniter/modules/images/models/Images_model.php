db267f5c4d057cabbe99840158663b5508e4d894
Reconix <Reconix@users.noreply.github.com>
2019-05-29 07:10:10 +0100

    images update
    try PHP Coding Standards to follow PSR's

--- ForgeIgniter/modules/images/models/Images_model.php
+++ ForgeIgniter/modules/images/models/Images_model.php
@@ -13,142 +13,120 @@
  * @since		Hal Version 1.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // ------------------------------------------------------------------------
 
-class Images_model extends CI_Model {
+class Images_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
 
-	function __construct()
-	{
-		parent::__construct();
+        // get siteID, if available
+        if (defined('SITEID')) {
+            $this->siteID = SITEID;
+        }
+    }
 
-		// get siteID, if available
-		if (defined('SITEID'))
-		{
-			$this->siteID = SITEID;
-		}
-	}
+    public function search_images($query, $limit = '')
+    {
+        if (!$query) {
+            return false;
+        }
 
-	function search_images($query, $limit = '')
-	{
-		if (!$query)
-		{
-			return FALSE;
-		}
+        $this->db->where(array('siteID' => $this->siteID, 'deleted' => 0));
 
-		$this->db->where(array('siteID' => $this->siteID, 'deleted' => 0));
+        $this->db->where('(imageRef LIKE "%'.$this->db->escape_like_str($query).'%" OR imageName LIKE "%'.$this->db->escape_like_str($query).'%")');
 
-		$this->db->where('(imageRef LIKE "%'.$this->db->escape_like_str($query).'%" OR imageName LIKE "%'.$this->db->escape_like_str($query).'%")');
+        $this->db->order_by('imageRef', 'asc');
 
-		$this->db->order_by('imageRef', 'asc');
+        $query = $this->db->get('images', $limit);
 
-		$query = $this->db->get('images', $limit);
+        if ($query->num_rows()) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
 
-		if ($query->num_rows())
-		{
-			return $query->result_array();
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public function get_images_by_folder_ref($ref, $limit = '')
+    {
+        $this->db->where(array('images.siteID' => $this->siteID, 'images.deleted' => 0));
 
-	function get_images_by_folder_ref($ref, $limit = '')
-	{
-		$this->db->where(array('images.siteID' => $this->siteID, 'images.deleted' => 0));
+        $this->db->where('folderSafe', $ref);
 
-		$this->db->where('folderSafe', $ref);
+        $this->db->select('images.*, folderName, folderSafe');
+        $this->db->join('image_folders', 'image_folders.folderID = images.folderID');
 
-		$this->db->select('images.*, folderName, folderSafe');
-		$this->db->join('image_folders', 'image_folders.folderID = images.folderID');
+        $this->db->order_by('imageRef', 'asc');
 
-		$this->db->order_by('imageRef', 'asc');
+        $query = $this->db->get('images', $limit);
 
-		$query = $this->db->get('images', $limit);
+        if ($query->num_rows()) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
 
-		if ($query->num_rows())
-		{
-			return $query->result_array();
-		}
-		else
-		{
-			return false;
-		}
-	}
+    public function get_folders($folderID = '')
+    {
+        // default where
+        $this->db->where(array('siteID' => $this->siteID, 'deleted' => 0));
+        $this->db->order_by('folderOrder');
 
-	function get_folders($folderID = '')
-	{
-		// default where
-		$this->db->where(array('siteID' => $this->siteID, 'deleted' => 0));
-		$this->db->order_by('folderOrder');
+        // get based on folder ID
+        if ($folderID) {
+            $query = $this->db->get_where('image_folders', array('folderID' => $folderID), 1);
 
-		// get based on folder ID
-		if ($folderID)
-		{
-			$query = $this->db->get_where('image_folders', array('folderID' => $folderID), 1);
+            if ($query->num_rows()) {
+                return $query->row_array();
+            } else {
+                return false;
+            }
+        }
+        // or just get all of em
+        else {
+            $query = $this->db->get('image_folders');
 
-			if ($query->num_rows())
-			{
-				return $query->row_array();
-			}
-			else
-			{
-				return FALSE;
-			}
-		}
-		// or just get all of em
-		else
-		{
-			$query = $this->db->get('image_folders');
+            if ($query->num_rows()) {
+                return $query->result_array();
+            } else {
+                return false;
+            }
+        }
+    }
 
-			if ($query->num_rows())
-			{
-				return $query->result_array();
-			}
-			else
-			{
-				return FALSE;
-			}
-		}
-	}
+    public function lookup_user($userID, $display = false)
+    {
+        // default wheres
+        $this->db->where('userID', $userID);
 
-	function lookup_user($userID, $display = FALSE)
-	{
-		// default wheres
-		$this->db->where('userID', $userID);
+        // grab
+        $query = $this->db->get('users', 1);
 
-		// grab
-		$query = $this->db->get('users', 1);
+        if ($query->num_rows()) {
+            $row = $query->row_array();
 
-		if ($query->num_rows())
-		{
-			$row = $query->row_array();
+            if ($display !== false) {
+                return ($row['displayName']) ? $row['displayName'] : $row['firstName'].' '.$row['lastName'];
+            } else {
+                return $row;
+            }
+        } else {
+            return false;
+        }
+    }
 
-			if ($display !== FALSE)
-			{
-				return ($row['displayName']) ? $row['displayName'] : $row['firstName'].' '.$row['lastName'];
-			}
-			else
-			{
-				return $row;
-			}
-		}
-		else
-		{
-			return FALSE;
-		}
-	}
+    public function update_children($folderID)
+    {
+        // update page draft
+        $this->db->set('folderID', 0);
+        $this->db->where('siteID', $this->siteID);
+        $this->db->where('folderID', $folderID);
+        $this->db->update('images');
 
-	function update_children($folderID)
-	{
-		// update page draft
-		$this->db->set('folderID', 0);
-		$this->db->where('siteID', $this->siteID);
-		$this->db->where('folderID', $folderID);
-		$this->db->update('images');
-
-		return TRUE;
-	}
+        return true;
+    }
 }
