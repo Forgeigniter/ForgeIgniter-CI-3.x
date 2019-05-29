2493f399e0b9fc691ed36c3377a077f6eb614592
Reconix <Reconix@users.noreply.github.com>
2019-05-29 07:06:19 +0100

    forums update
    try PHP Coding Standards to follow PSR's

--- ForgeIgniter/modules/forums/controllers/Admin.php
+++ ForgeIgniter/modules/forums/controllers/Admin.php
@@ -13,242 +13,221 @@
  * @since		Hal Version 1.0
  * @filesource
  */
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 // ------------------------------------------------------------------------
 
-class Admin extends MX_Controller {
+class Admin extends MX_Controller
+{
 
-	// set defaults
-	var $includes_path = '/includes/admin';				// path to includes for header and footer
-	var $redirect = '/admin/forums/forums';				// default redirect
-	var $permissions = array();
+    // set defaults
+    public $includes_path = '/includes/admin';				// path to includes for header and footer
+    public $redirect = '/admin/forums/forums';				// default redirect
+    public $permissions = array();
 
-	function __construct()
-	{
-		parent::__construct();
+    public function __construct()
+    {
+        parent::__construct();
 
-		// check user is logged in, if not send them away from this controller
-		if (!$this->session->userdata('session_admin'))
-		{
-			redirect('/admin/login/'.$this->core->encode($this->uri->uri_string()));
-		}
+        // check user is logged in, if not send them away from this controller
+        if (!$this->session->userdata('session_admin')) {
+            redirect('/admin/login/'.$this->core->encode($this->uri->uri_string()));
+        }
 
-		// get permissions and redirect if they don't have access to this module
-		if (!$this->permission->permissions)
-		{
-			redirect('/admin/dashboard/permissions');
-		}
-		if (!in_array($this->uri->segment(2), $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+        // get permissions and redirect if they don't have access to this module
+        if (!$this->permission->permissions) {
+            redirect('/admin/dashboard/permissions');
+        }
+        if (!in_array($this->uri->segment(2), $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		//  load models and libs
-		$this->load->model('forums_model', 'forums');
-		$this->load->library('tags');
+        //  load models and libs
+        $this->load->model('forums_model', 'forums');
+        $this->load->library('tags');
 
-		// get siteID, if available
-		if (defined('SITEID'))
-		{
-			$this->siteID = SITEID;
-		}
-	}
+        // get siteID, if available
+        if (defined('SITEID')) {
+            $this->siteID = SITEID;
+        }
+    }
 
-	function index()
-	{
-		redirect($this->redirect);
-	}
+    public function index()
+    {
+        redirect($this->redirect);
+    }
 
-	function forums()
-	{
-		// grab data and display
-		$output = $this->core->viewall('forums');
+    public function forums()
+    {
+        // grab data and display
+        $output = $this->core->viewall('forums');
 
-		$this->load->view($this->includes_path.'/header');
-		$this->load->view('admin/forums',$output);
-		$this->load->view($this->includes_path.'/footer');
-	}
+        $this->load->view($this->includes_path.'/header');
+        $this->load->view('admin/forums', $output);
+        $this->load->view($this->includes_path.'/footer');
+    }
 
-	function add_forum()
-	{
-		// check permissions for this page
-		if (!in_array('forums_edit', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function add_forum()
+    {
+        // check permissions for this page
+        if (!in_array('forums_edit', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		// get values
-		$output['data'] = $this->core->get_values('forums');
-		$output['groups'] = $this->permission->get_groups();
+        // get values
+        $output['data'] = $this->core->get_values('forums');
+        $output['groups'] = $this->permission->get_groups();
 
-		// get categories
-		$output['categories'] = $this->forums->get_categories();
+        // get categories
+        $output['categories'] = $this->forums->get_categories();
 
-		if (count($_POST))
-		{
-			// required
-			$this->core->required = array(
-				'forumName' => array('label' => 'Forum Name', 'rules' => 'required|trim'),
-			);
+        if (count($_POST)) {
+            // required
+            $this->core->required = array(
+                'forumName' => array('label' => 'Forum Name', 'rules' => 'required|trim'),
+            );
 
-			// set date
-			$this->core->set['dateCreated'] = date("Y-m-d H:i:s");
+            // set date
+            $this->core->set['dateCreated'] = date("Y-m-d H:i:s");
 
-			// update
-			if ($this->core->update('forums'))
-			{
-				$forumID = $this->db->insert_id();
+            // update
+            if ($this->core->update('forums')) {
+                $forumID = $this->db->insert_id();
 
-				// where to redirect to
-				redirect($this->redirect);
-			}
-		}
+                // where to redirect to
+                redirect($this->redirect);
+            }
+        }
 
-		// templates
-		$this->load->view($this->includes_path.'/header');
-		$this->load->view('admin/add_forum', $output);
-		$this->load->view($this->includes_path.'/footer');
-	}
+        // templates
+        $this->load->view($this->includes_path.'/header');
+        $this->load->view('admin/add_forum', $output);
+        $this->load->view($this->includes_path.'/footer');
+    }
 
-	function edit_forum($forumID)
-	{
-		// check permissions for this page
-		if (!in_array('forums_edit', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function edit_forum($forumID)
+    {
+        // check permissions for this page
+        if (!in_array('forums_edit', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		// set object ID
-		$objectID = array('forumID' => $forumID);
+        // set object ID
+        $objectID = array('forumID' => $forumID);
 
-		// get values
-		$output['data'] = $this->core->get_values('forums', $objectID);
-		$output['groups'] = $this->permission->get_groups();
+        // get values
+        $output['data'] = $this->core->get_values('forums', $objectID);
+        $output['groups'] = $this->permission->get_groups();
 
-		// get categories
-		$output['categories'] = $this->forums->get_categories();
+        // get categories
+        $output['categories'] = $this->forums->get_categories();
 
-		if (count($_POST))
-		{
-			// required
-			$this->core->required = array(
-				'forumName' => array('label' => 'forum Name', 'rules' => 'required|trim'),
-			);
+        if (count($_POST)) {
+            // required
+            $this->core->required = array(
+                'forumName' => array('label' => 'forum Name', 'rules' => 'required|trim'),
+            );
 
-			// set date
-			$this->core->set['dateModified'] = date("Y-m-d H:i:s");
+            // set date
+            $this->core->set['dateModified'] = date("Y-m-d H:i:s");
 
-			// update
-			if ($this->core->update('forums', $objectID))
-			{
-				// where to redirect to
-				redirect($this->redirect);
-			}
-		}
+            // update
+            if ($this->core->update('forums', $objectID)) {
+                // where to redirect to
+                redirect($this->redirect);
+            }
+        }
 
-		// templates
-		$this->load->view($this->includes_path.'/header');
-		$this->load->view('admin/edit_forum', $output);
-		$this->load->view($this->includes_path.'/footer');
-	}
+        // templates
+        $this->load->view($this->includes_path.'/header');
+        $this->load->view('admin/edit_forum', $output);
+        $this->load->view($this->includes_path.'/footer');
+    }
 
-	function delete_forum($objectID)
-	{
-		// check permissions for this page
-		if (!in_array('forums_delete', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function delete_forum($objectID)
+    {
+        // check permissions for this page
+        if (!in_array('forums_delete', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		if ($this->core->soft_delete('forums', array('forumID' => $objectID)))
-		{
-			// where to redirect to
-			redirect($this->redirect);
-		}
-	}
+        if ($this->core->soft_delete('forums', array('forumID' => $objectID))) {
+            // where to redirect to
+            redirect($this->redirect);
+        }
+    }
 
-	function categories()
-	{
-		// check permissions for this page
-		if (!in_array('forums_cats', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function categories()
+    {
+        // check permissions for this page
+        if (!in_array('forums_cats', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		// required fields
-		$this->core->required = array('catName' => 'Category name');
+        // required fields
+        $this->core->required = array('catName' => 'Category name');
 
-		// set date
-		$this->core->set['dateCreated'] = date("Y-m-d H:i:s");
+        // set date
+        $this->core->set['dateCreated'] = date("Y-m-d H:i:s");
 
-		// get values
-		$output = $this->core->get_values('forums_cats');
+        // get values
+        $output = $this->core->get_values('forums_cats');
 
-		// update
-		if ($this->core->update('forums_cats') && count($_POST))
-		{
-			// where to redirect to
-			redirect('/admin/forums/categories');
-		}
+        // update
+        if ($this->core->update('forums_cats') && count($_POST)) {
+            // where to redirect to
+            redirect('/admin/forums/categories');
+        }
 
-		$output['categories'] = $this->forums->get_categories();
+        $output['categories'] = $this->forums->get_categories();
 
-		$this->load->view($this->includes_path.'/header');
-		$this->load->view('admin/categories',$output);
-		$this->load->view($this->includes_path.'/footer');
-	}
+        $this->load->view($this->includes_path.'/header');
+        $this->load->view('admin/categories', $output);
+        $this->load->view($this->includes_path.'/footer');
+    }
 
-	function edit_cat()
-	{
-		// check permissions for this page
-		if (!in_array('forums_cats', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function edit_cat()
+    {
+        // check permissions for this page
+        if (!in_array('forums_cats', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		// go through post and edit each list item
-		$listArray = $this->core->get_post();
-		if (count($listArray))
-		{
-			foreach($listArray as $ID => $value)
-			{
-				if ($ID != '' && sizeof($value) > 0)
-				{
-					// set object ID
-					$objectID = array('catID' => $ID);
-					$this->core->set['catName'] = $value['catName'];
-					$this->core->update('forums_cats', $objectID);
-				}
-			}
-		}
+        // go through post and edit each list item
+        $listArray = $this->core->get_post();
+        if (count($listArray)) {
+            foreach ($listArray as $ID => $value) {
+                if ($ID != '' && sizeof($value) > 0) {
+                    // set object ID
+                    $objectID = array('catID' => $ID);
+                    $this->core->set['catName'] = $value['catName'];
+                    $this->core->update('forums_cats', $objectID);
+                }
+            }
+        }
 
-		// where to redirect to
-		redirect('/admin/forums/categories');
-	}
+        // where to redirect to
+        redirect('/admin/forums/categories');
+    }
 
-	function delete_cat($catID)
-	{
-		// check permissions for this page
-		if (!in_array('forums_cats', $this->permission->permissions))
-		{
-			redirect('/admin/dashboard/permissions');
-		}
+    public function delete_cat($catID)
+    {
+        // check permissions for this page
+        if (!in_array('forums_cats', $this->permission->permissions)) {
+            redirect('/admin/dashboard/permissions');
+        }
 
-		// where
-		$objectID = array('catID' => $catID);
+        // where
+        $objectID = array('catID' => $catID);
 
-		if ($this->core->soft_delete('forums_cats', $objectID))
-		{
-			// where to redirect to
-			redirect('/admin/forums/categories');
-		}
-	}
+        if ($this->core->soft_delete('forums_cats', $objectID)) {
+            // where to redirect to
+            redirect('/admin/forums/categories');
+        }
+    }
 
-	function order($field = '')
-	{
-		$this->core->order(key($_POST), $field);
-	}
-
-
+    public function order($field = '')
+    {
+        $this->core->order(key($_POST), $field);
+    }
 }
