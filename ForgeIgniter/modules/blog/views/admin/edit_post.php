6bfde42608de805c530bcb74b63289bd910e8a0e
Reconix <Reconix@users.noreply.github.com>
2022-10-15 23:51:39 +0100

    Admin - Blog - Edit Post - Custom Tabs
    We've also setup tabs for SEO, Image and Gallery.
    
    There should defiantly be a blog settings page where we can set if a blog post has a page header image that's different to the main post image, also if you'd like to add a Gallery.

--- ForgeIgniter/modules/blog/views/admin/edit_post.php
+++ ForgeIgniter/modules/blog/views/admin/edit_post.php
@@ -108,86 +108,131 @@ $(function(){
 				</div>
 				<?php endif; ?>
 
-				<!-- Posts -->
-				<div class="box box-crey">
-					<div class="box-header with-border">
-						<i class="fa fa-edit"></i>
-						<h3 class="box-title">Edit Blog Post</h3>
-					</div>
-					<div class="box-body" style="padding:20px">
+				<!-- Edit Post - Custom Nav Box -->
+				<div class="box box-crey nav-tabs-custom-box">
+					<ul class="nav nav-tabs pull-right">
+						<li class="pull-left header box-title"><i class="fa fa-edit"></i>Edit Blog Post</li>
+						<li class=""><a href="#tab_gallery" data-toggle="tab" aria-expanded="false">Gallery</a></li>
+						<li class=""><a href="#tab_images" data-toggle="tab" aria-expanded="false">Images</a></li>
+						<li class=""><a href="#tab_seo" data-toggle="tab" aria-expanded="false">SEO</a></li>
+						<li class=""><a href="#tab_content" data-toggle="tab" aria-expanded="false">Content</a></li>
+						<li class="active"><a href="#tab_details" data-toggle="tab" aria-expanded="true">Details</a></li>
+					</ul>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<div class="tab-content">
 
-					<label for="postName">Title:</label>
-					<?php echo form_input('postTitle', set_value('postTitle', $data['postTitle']), 'id="postTitle" class="form-control" style="width:50%;"'); ?>
-					<br class="clear" />
+							<div class="tab-pane active" id="tab_details" style="padding:10px 8px">
+								<label for="postName">Title:</label>
+								<?php echo form_input('postTitle', set_value('postTitle', $data['postTitle']), 'id="postTitle" class="form-control" style="width:50%;"'); ?>
 
-					<label>Categories: <small>[<a href="<?php echo site_url('/admin/blog/categories'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
-					<div class="categories">
-						<?php if ($categories): ?>
-						<?php foreach ($categories as $category): ?>
-							<div class="category<?php echo (isset($data['categories'][$category['catID']])) ? ' hover' : ''; ?>">
-								<?php echo form_checkbox('catsArray['.$category['catID'].']', $category['catName'], (isset($data['categories'][$category['catID']])) ? 1 : ''); ?><span><?php echo $category['catName']; ?></span>
+								<div class="autosave">
+									<label for="excerpt">Introduction (Excerpt):</label>
+									<?php
+				            $options = [
+				                'name'        => 'excerpt',
+				                'id'          => 'excerpt',
+				                'value'       => set_value('excerpt', $data['excerpt']),
+				                'rows'        => '5',
+				                'cols'        => '10',
+				                'style'       => 'width:50%; margin-right:10px;',
+				                'class'       => 'form-control formelement code short'
+				            ];
+				            echo form_textarea($options);
+			            ?>
+									<div class="previewExcerpt"></div>
+								</div>
+
+								<label>Categories: <small>[<a href="<?php echo site_url('/admin/blog/categories'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')">update</a>]</small></label>
+								<div class="categories">
+									<?php if ($categories): ?>
+									<?php foreach ($categories as $category): ?>
+										<div class="category<?php echo (isset($data['categories'][$category['catID']])) ? ' hover' : ''; ?>">
+											<?php echo form_checkbox('catsArray['.$category['catID'].']', $category['catName'], (isset($data['categories'][$category['catID']])) ? 1 : ''); ?><span><?php echo $category['catName']; ?></span>
+										</div>
+									<?php endforeach; ?>
+									<?php else: ?>
+										<div class="category">
+											<strong>Warning:</strong> It is strongly recommended that you use categories or this may not appear properly. <a href="<?php echo site_url('/admin/blog/categories'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')"><strong>Please update your categories here</strong></a>.
+										</div>
+									<?php endif; ?>
+								</div>
+
 							</div>
-						<?php endforeach; ?>
-						<?php else: ?>
-							<div class="category">
-								<strong>Warning:</strong> It is strongly recommended that you use categories or this may not appear properly. <a href="<?php echo site_url('/admin/blog/categories'); ?>" onclick="return confirm('You will lose any unsaved changes.\n\nContinue anyway?')"><strong>Please update your categories here</strong></a>.
+
+							<div class="tab-pane" id="tab_content">
+
+								<div class="autosave">
+									<label for="body">Content (Body):</label>
+
+									<br class="clear" />
+
+									<div class="buttons">
+										<a href="#" class="boldbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_bold.png" alt="Bold" title="Bold" /></a>
+										<a href="#" class="italicbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_italic.png" alt="Italic" title="Italic" /></a>
+										<a href="#" class="h1button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_h1.png" alt="Heading 1" title="Heading 1"/></a>
+										<a href="#" class="h2button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_h2.png" alt="Heading 2" title="Heading 2" /></a>
+										<a href="#" class="h3button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_h3.png" alt="Heading 3" title="Heading 3" /></a>
+										<a href="#" class="urlbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_url.png" alt="Insert Link" title="Insert Link" /></a>
+										<a href="<?php echo site_url('/admin/images/browser'); ?>" class="ficms_imagebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_image.png" alt="Insert Image" title="Insert Image" /></a>
+										<a href="<?php echo site_url('/admin/files/browser'); ?>" class="ficms_filebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_file.png" alt="Insert File" title="Insert File" /></a>
+										<a href="#" class="previewbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/icons/cms/btn_save.png" alt="Preview" title="Preview" /></a>
+									</div>
+
+									<br class="clear" />
+
+									<?php
+				            $options = [
+				                    'name'        => 'body',
+				                    'id'          => 'body',
+				                    'value'       => set_value('body', $data['body']),
+				                    'rows'        => '30',
+				                    'cols'        => '10',
+				                    'style'       => 'width:50%; margin-right:10px;',
+				                    'class'       => 'form-control formelement code half'
+				            ];
+				            echo form_textarea($options);
+									?>
+									<div class="preview"></div>
+								</div>
+
+							</div><!-- /.Content Tab -->
+
+							<div class="tab-pane" id="tab_seo">
+								keywords: <br>
+								description: <br>
+								Robots <br>
 							</div>
-						<?php endif; ?>
-					</div>
-					<br class="clear" />
 
-					<div class="autosave">
-						<label for="excerpt">Introduction (Excerpt):</label>
-						<?php
-	            $options = [
-	                'name'        => 'excerpt',
-	                'id'          => 'excerpt',
-	                'value'       => set_value('excerpt', $data['excerpt']),
-	                'rows'        => '5',
-	                'cols'        => '10',
-	                'style'       => 'width:50%; margin-right:10px;',
-	                'class'       => 'form-control formelement code short'
-	            ];
-	            echo form_textarea($options);
-            ?>
-						<div class="previewExcerpt"></div>
-					</div>
+							<div class="tab-pane" id="tab_images">
 
-					<br class="clear" />
+								<label for="header-img">Header Image</label>
 
-					<div class="buttons">
-						<a href="#" class="boldbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_bold.png" alt="Bold" title="Bold" /></a>
-						<a href="#" class="italicbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_italic.png" alt="Italic" title="Italic" /></a>
-						<a href="#" class="h1button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h1.png" alt="Heading 1" title="Heading 1"/></a>
-						<a href="#" class="h2button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h2.png" alt="Heading 2" title="Heading 2" /></a>
-						<a href="#" class="h3button"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_h3.png" alt="Heading 3" title="Heading 3" /></a>
-						<a href="#" class="urlbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_url.png" alt="Insert Link" title="Insert Link" /></a>
-						<a href="<?php echo site_url('/admin/images/browser'); ?>" class="ficms_imagebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_image.png" alt="Insert Image" title="Insert Image" /></a>
-						<a href="<?php echo site_url('/admin/files/browser'); ?>" class="ficms_filebutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_file.png" alt="Insert File" title="Insert File" /></a>
-						<a href="#" class="previewbutton"><img src="<?php echo base_url() . $this->config->item('staticPath'); ?>/images/btn_save.png" alt="Preview" title="Preview" /></a>
-					</div>
+								<label for="thumb-img">Thumbnail Image</label>
+								<div class="form-group" x-data="{ fileName: '' }">
+									<div class="input-group shadow">
+										<span class="input-group-text px-3 text-muted"><i class="fas fa-image fa-lg"></i></span>
+										<input type="file" x-ref="file" @change="fileName = $refs.file.files[0].name" name="img[]" class="d-none">
+										<input type="text" class="form-control form-control-lg" placeholder="Upload Image" x-model="fileName">
+										<button class="browse btn btn-primary px-4" type="button" x-on:click.prevent="$refs.file.click()"><i class="fas fa-image"></i> Browse</button>
+									</div>
+								</div>
 
-					<div class="autosave">
-						<label for="body">Content (Body):</label>
-						<?php
-	            $options = [
-	                    'name'        => 'body',
-	                    'id'          => 'body',
-	                    'value'       => set_value('body', $data['body']),
-	                    'rows'        => '30',
-	                    'cols'        => '10',
-	                    'style'       => 'width:50%; margin-right:10px;',
-	                    'class'       => 'form-control formelement code half'
-	            ];
-	            echo form_textarea($options);
-						?>
-						<div class="preview"></div>
-					</div>
-					<br class="clear" /><br />
+								<label for="gallery">Gallery Images</label>
 
-					</div><!-- /.box-body  -->
-				</div>
-			</div>
+
+							</div><!-- /. Tab Images -->
+
+							<div class="tab-pane" id="tab_gallery">
+								Test
+							</div>
+
+						</div><!-- /.tab-content -->
+					</div><!-- /.box-body -->
+				</div><!-- Custom Nav Box -->
+
+
+			</div><!-- /.col-md-9 -->
 
 			<!-- Right Sidebar -->
 			<div class="col-md-3">
