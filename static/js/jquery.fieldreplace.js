21ae06262e04a79b1bff29a4051a77e48ad9d6dd
Reconix <Reconix@users.noreply.github.com>
2023-03-26 23:36:46 +0100

    Delete static directory

--- static/js/jquery.fieldreplace.js
+++
@@ -1,9 +0,0 @@
-/**
- * jQuery.FieldReplacer - Simple Field Replacer Plugin
- * Copyright (c) 2009-2011 Stu Green - Haloweb Ltd - http://www.haloweb.co.uk
- * Licensed under MIT and GPL.
- * Date: 01/11/2009
- * @author Stu Green
- * @version 0.9
- **/
-;eval(function(p,a,c,k,e,r){e=function(c){return c.toString(a)};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('(2($){$.g.h=2(d){4 e=0;$(e).7(2(){4 a=0;4 b=\'\';4 c=$(a).5(\'6\');3(d){b=d}i{b=$(a).8(\'9\')}3($(a).1()==\'\'){$(a).1(b);$(a).5({6:\'#f\'})}$(a).j(2(){3($(0).1()==b){$(a).5({6:c});$(0).1(\'\')}});$(a).k(2(){3($(0).1()==\'\'){$(a).5({6:\'#f\'});$(0).1(b)}})});$(l).m(\'n\',2(){$(e).7(2(){3($(0).1()!=\'\'&&($(0).1()==$(0).8(\'9\')||$(0).1()==d)){$(0).1(\'\')}o p})})}})(q);',27,27,'this|val|function|if|var|css|color|each|attr|title||||||bbb|fn|fieldreplace|else|focus|blur|window|bind|beforeunload|return|true|jQuery'.split('|'),0,{}));
