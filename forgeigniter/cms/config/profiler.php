6b9f586314420c89d7a4f22a25126ebb52de9163
Reconix <Reconix@users.noreply.github.com>
2023-04-06 23:11:26 +0100

    Dev Profiler Bar ( Inspired by PHPQuickProfiler )
    This is originally by Lonnie, I'm pretty sure bonfire has a better looking one and obviously CI4 version has a nice one too.
    
    I'm pretty sure there's some PHP8.2 stuff I've not fixed but atm, we have a cool bar !

---
+++ forgeigniter/cms/config/profiler.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+| -------------------------------------------------------------------------
+| Profiler Sections
+| -------------------------------------------------------------------------
+| This file lets you determine whether or not various sections of Profiler
+| data are displayed when the Profiler is enabled.
+| Please see the user guide for info:
+|
+|	https://codeigniter.com/userguide3/general/profiling.html
+|
+*/
+
+$config['benchmarks']           = TRUE;
+$config['config']               = TRUE;
+$config['controller_info']      = TRUE;
+$config['get']                  = TRUE;
+$config['http_headers']         = TRUE;
+$config['memory_usage']         = TRUE;
+$config['post']                 = TRUE;
+$config['queries']              = TRUE;
+$config['eloquent']             = FALSE;
+$config['uri_string']           = TRUE;
+$config['view_data']            = TRUE;
+$config['query_toggle_count']   = 50;
+
+// Other Possible configs I've missed ?
+
+// $config['session_data'] = FALSE;
+// $config['cookie_data'] = FALSE;
