f326eb1fd15fe46cf1e7308f0f8a938903e1adef
Reconix <Reconix@users.noreply.github.com>
2023-08-06 23:43:13 +0100

    Let Admin_Controller see multiple view module locations

--- forgeigniter/cms/controllers/Admin_Controller.php
+++ forgeigniter/cms/controllers/Admin_Controller.php
@@ -6,8 +6,25 @@ class Admin_Controller extends MX_Controller {
         parent::__construct();
 
         // Set the default view path for admin controllers
+
         $module = $this->router->fetch_module();  // Get current module
-        $this->load->set_view_path(APPPATH.'modules/'.$module.'/views/admin/');
+
+        // Since we have multiple module locations
+        $module_path = NULL;
+        $module_locations = $this->config->item('modules_locations');
+        foreach ($module_locations as $location) {
+            if (is_dir($location . $module)) {
+                $module_path = $location . $module;
+                break;
+            }
+        }
+
+        if ($module_path) {
+            $this->load->set_view_path($module_path . '/views/admin/');
+        } else {
+            log_message('error', 'Admin Module view path not found for: ' . $module);
+        }
+
     }
 
 }
